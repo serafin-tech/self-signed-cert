c0c34667d60cae2f7ae7a37226fda39606ff3c0c
Jaroslaw Wencel <jaroslaw.wencel@gmail.com>
2024-03-24 18:54:33 +0100

    Added alliases processing to the cert

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-# self-signed-cert.sh
+# `self-signed-cert.sh`
 
 Script for generating self-signed certificate with subjectAltName extension
+
+## How to use?
+
+1. Fill details in `self-signed-cert.sh`
+2. Run the script
 
--- self-signed-cert.sh
+++ self-signed-cert.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+### change me ###
+CN=common.name.crt
+ALIASES="alias1.name.crt alias2.name.crt"
+IP=1.2.3.4
+#################
+
 FILE_NAME=self-signed-cert
 
-CN=common.name.crt
-SANVAL="DNS:${CN},IP:1.2.3.4"
+SANVAL="DNS:${CN},IP:${IP}"
+for item in $ALIASES
+do
+  SANVAL="${SANVAL},DNS:${item}"
+done
 
 openssl req -new -x509 -days 3650 \
   -out ${FILE_NAME}.crt \
