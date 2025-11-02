1405159b43724ab73bb32037e2c7986b2dbf4fef
Jaroslaw Wencel <jaroslaw@wencel.it>
2025-11-02 12:42:09 +0100

    shellcheck finding fixes

--- self-signed-cert.sh
+++ self-signed-cert.sh
@@ -9,16 +9,16 @@ IP=1.2.3.4
 FILE_NAME=self-signed-cert
 
 SANVAL="DNS:${CN},IP:${IP}"
-for item in $ALIASES
+for item in ${ALIASES}
 do
   SANVAL="${SANVAL},DNS:${item}"
 done
 
 openssl req -new -x509 -days 3650 \
-  -out ${FILE_NAME}.crt \
-  -newkey rsa:2048 -noenc -keyout ${FILE_NAME}.key \
+  -out "${FILE_NAME}.crt" \
+  -newkey rsa:2048 -noenc -keyout "${FILE_NAME}.key" \
   -subj "/CN=${CN}" \
   -addext "subjectAltName=${SANVAL}"
 
-openssl x509 -in ${FILE_NAME}.crt -text
+openssl x509 -in "${FILE_NAME}.crt" -text
 
